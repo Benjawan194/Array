9af3bafc7ea738b8398d2a682c70410a700291e8
Benjawan194 <p.o.t.peang@gmail.com>
2017-04-10 12:50:47 +0700

    good

---
+++ src/Arraylis/App.java
@@ -0,0 +1,21 @@
+package Arraylis;
+import java.util.ArrayList;
+
+public class App {
+
+	public static void main(String[] args) {
+		 ArrayList lst = new ArrayList();
+		 lst.add(0,1);
+		 lst.add(1,2);
+		 lst.add(2,3);
+		 lst.add(3,4);
+		 lst.add(4,5);
+		 
+		 System.out.println(lst);
+		 System.out.print(lst.size);
+		 
+		 
+
+	}
+
+}
